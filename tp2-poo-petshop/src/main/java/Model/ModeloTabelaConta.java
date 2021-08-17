179fbb0d3aebe243e37add4edf919d44addd7363
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2021-08-16 23:23:17 -0300

    Criando modelos de visualização de dados

---
+++ tp2-poo-petshop/src/main/java/Model/ModeloTabelaConta.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Classes.Conta;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author lucas
+ */
+public class ModeloTabelaConta extends AbstractTableModel {
+
+    private ArrayList<Conta> listaContas;
+
+    public ModeloTabelaConta() {
+        this.listaContas = new ArrayList<>();
+    }
+
+    public ArrayList<Conta> getListaContas() {
+        return listaContas;
+    }
+
+    public void setListaContas(ArrayList<Conta> listaContas) {
+        this.listaContas = listaContas;
+    }
+
+    
+    @Override
+    public int getRowCount() {
+        return this.listaContas.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public Object getValueAt(int linha, int coluna) {
+        switch(coluna){
+            case 0:{
+                return this.listaContas.get(linha).getDataPagamento();
+            }
+            case 1:{
+                return this.listaContas.get(linha).getDataVencimento();
+            }
+             case 2:{
+                return this.listaContas.get(linha).getDescricao();
+            }
+            case 3:{
+                return this.listaContas.get(linha).getValor();
+            }   
+        }
+        return null;
+    }
+
+    @Override
+    public String getColumnName(int coluna) {
+        switch(coluna){
+            case 0:{
+                return "Pagamento";
+            }
+            case 1:{
+                return "Vencimento";
+            }
+             case 2:{
+                return "Descrição";
+            }
+            case 3:{
+                return "Preço";
+            }   
+        }
+        return null;
+    }
+    
+    
+}
---
+++ tp2-poo-petshop/src/main/java/Model/ModeloTabelaOrdemServico.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Classes.OrdemServico;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author lucas
+ */
+public class ModeloTabelaOrdemServico extends AbstractTableModel {
+
+    private ArrayList<OrdemServico> listaOrdens;
+    private boolean incluirObservacao;
+
+    public ModeloTabelaOrdemServico(boolean incluirObservacao) {
+        this.listaOrdens = new ArrayList<>();
+        this.incluirObservacao = incluirObservacao;
+    }
+
+    public ArrayList<OrdemServico> getListaOrdens() {
+        return listaOrdens;
+    }
+
+    public void setListaOrdens(ArrayList<OrdemServico> listaOrdens) {
+        this.listaOrdens = listaOrdens;
+    }
+
+    public boolean isIncluirObservacao() {
+        return incluirObservacao;
+    }
+
+    public void setIncluirObservacao(boolean incluirObservacao) {
+        this.incluirObservacao = incluirObservacao;
+    }
+
+    
+    @Override
+    public int getRowCount() {
+        return this.listaOrdens.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.incluirObservacao ? 5 : 4;
+    }
+
+    @Override
+    public Object getValueAt(int linha, int coluna) {
+        switch(coluna){
+            case 0:{
+                return this.listaOrdens.get(linha).getId();
+            }
+            case 1:{
+                return this.listaOrdens.get(linha).getServico();
+            }
+             case 2:{
+                return this.listaOrdens.get(linha).getServico();
+            }
+             case 3:{
+                return this.listaOrdens.get(linha).getData();
+            }
+             case 4:{
+                return this.incluirObservacao? this.listaOrdens.get(linha).getObservacao() : null;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getColumnName(int coluna) {
+        switch(coluna){
+            case 0:{
+                return "Id";
+            }
+            case 1:{
+                return "Id serviço";
+            }
+             case 2:{
+                return "Serviço";
+            }
+             case 3:{
+                return "Data";
+            }
+             case 4:{
+                return this.incluirObservacao ? "Tratamento" : null;
+            }
+        }
+        return null;
+    }
+    
+    
+}
